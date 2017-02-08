ac731243ce0d211ab1bf8b798662c65b147430c6
Captain Lin <Captain Lin>
2017-02-07 22:10:32 -0800

    Set up initial routing

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Module
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// Routes
+weatherApp.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'homeController' 
+	})
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+	 	controller: 'forecastController' 
+	});
+});
+
+weatherApp.controller('homeController', ['$scope', function ($scope) {
+
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function ($scope) {
+
+
+}]);
