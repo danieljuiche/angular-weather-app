1c8eb289770475f0739418c68bc911b0f3c6d339
Captain Lin <Captain Lin>
2017-02-10 12:16:29 -0800

    Added date and temperature conversions and routing for forecast view

--- js/app.js
+++ js/app.js
@@ -11,16 +11,20 @@ weatherApp.config(function ($routeProvider, $sceDelegateProvider) {
 	.when('/forecast', {
 		templateUrl: 'pages/forecast.html',
 	 	controller: 'forecastController'
-	});
+	})
+	.when('/forecast/:days', {
+		templateUrl: 'pages/forecast.html',
+	 	controller: 'forecastController'
+	})
 
-	// Whitelist
+	// Whitelist the API
 	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://api.openweathermap.org/data/2.5/forecast/daily?APPID=9f7c06d0eb23c68b0fc17fec2280429b']);
 });
 
 
 weatherApp.service('cityNameService', function () {
+	// Default city
 	this.city = 'New York, NY';
-
 });
 
 weatherApp.controller('homeController', ['$scope', 'cityNameService', function ($scope, cityNameService) {
@@ -36,14 +40,28 @@ weatherApp.controller('homeController', ['$scope', 'cityNameService', function (
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityNameService', function ($scope, $resource, cityNameService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityNameService', function ($scope, $resource, $routeParams, cityNameService) {
 	
+	// Store initial variables
 	$scope.city = cityNameService.city;
+	$scope.daysToDisplay = $routeParams.days || 3;
 
+	// Get data from OpenWeatherMap API
 	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=9f7c06d0eb23c68b0fc17fec2280429b", { get: { method: "JSONP" }});
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.daysToDisplay });
 
-	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
 
+	// Function to convert temperature from Kelvin to Celcius
+	$scope.convertToCelcius = function (degK) {
+		return Math.round(degK - 273.15);
+	}
+
+	// Function to covert date from Unix to standard notation
+	$scope.convertToDate = function (unixTime) {
+		return new Date(unixTime * 1000);
+	}
+
+	// Console log data results
 	console.log($scope.weatherResult);
 
 }]);
